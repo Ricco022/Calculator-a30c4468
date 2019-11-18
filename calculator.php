15d7f5d9b39e8598db4d3fbe2cf454100e94af62
Riccardo van der Horst <riccardo271002@gmail.com>
2019-11-18 16:04:37 +0100

    Uitwerking van de exercise

---
+++ calculator.php
@@ -0,0 +1,15 @@
+<?php
+
+$mp = Readline ("Welke operatie wil je uitvoeren? (+, -)");
+
+$n1 = Readline ("Eerste getal?");
+
+$n2 = Readline ("Tweede getal?");
+
+    if ($mp == "+")
+    echo ($n1 + $n2);
+
+    if ($mp == "-")
+    echo ($n1 - $n2);
+
+?>
